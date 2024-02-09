f3755423ca96497ec8d3dacbd4236679e4cd5c09
Juan Mendoza <cebo_052000@hotmail.com>
2024-02-09 10:38:56 -0500

    update prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 60,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  arrowParens: 'always',
+
+  importOrder: [
+    '^react$',
+    '^next(/.*)?$',
+    '^lib/(.*)$',
+    '<THIRD_PARTY_MODULES>',
+    '^~/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+};
+
+export default prettierConfig;
